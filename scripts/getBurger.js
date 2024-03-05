6a2f2ffa8b2152989ff82d95e3dc70a8299f0635
raulsouza7 <csouzaraul@gmail.com>
2024-03-04 22:15:14 -0700

    burger button

---
+++ scripts/getBurger.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger-menu');
+    const navUL = document.getElementById('navbar').querySelector('ul');
+
+    hamburger.addEventListener('click', function () {
+        navUL.classList.toggle('show');
+
+        if (navUL.classList.contains('show')) {
+            this.innerHTML = '&times;';
+        } else {
+            this.innerHTML = '&#9776;';
+        }
+    });
+});
