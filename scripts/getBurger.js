094394d388f17dffd222092ffc0d87c1077a5de4
raulsouza7 <csouzaraul@gmail.com>
2024-03-04 22:25:29 -0700

    fixing burger button

--- scripts/getBurger.js
+++ scripts/getBurger.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger-menu');
     const navUL = document.getElementById('navbar').querySelector('ul');
 
-    hamburger.addEventListener('click', function () {
+    hamburger.addEventListener('click', function() {
         navUL.classList.toggle('show');
 
         if (navUL.classList.contains('show')) {
