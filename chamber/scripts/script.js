a439bfb337cfe80b4b637b42404a2192d49fd355
raulsouza7 <csouzaraul@gmail.com>
2024-03-06 14:28:53 -0700

    fixing mobile

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,16 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('nav-active');
-        hamburger.classList.toggle('open');
-    });
-
-    navLinks.addEventListener('click', (event) => {
-        if (event.target.tagName === 'A') {
-            navLinks.classList.remove('nav-active');
-            hamburger.classList.remove('open');
-        }
+    const lastModified = document.getElementById('last-updated');
+    lastModified.textContent = new Date(document.lastModified).toLocaleDateString('en-US', {
+        year: 'numeric', month: 'long', day: 'numeric', hour: '12', minute: '2-digit', second: '2-digit'
     });
 });
